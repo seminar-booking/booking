25c3a5ca5436b0930b4e3bd388008ac484a58aff
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2018-01-21 01:03:02 +0900

    Initial Push

---
+++ member-service/src/main/java/com/example/member/MemberServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberServiceApplication.class, args);
+	}
+}
