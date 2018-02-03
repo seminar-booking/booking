b40e82f22b18ab736c5ed86a61d860f83f42afa8
배철한 <joker10421@gmail.com>
2018-02-03 23:28:23 +0900

    member project dto commit (변수명 수정, toString, equals, hashcode 메서드 오버라이드)

--- member-service/src/main/java/com/example/member/TempPassword.java
+++ member-service/src/main/java/com/example/member/TempPassword.java
@@ -1,27 +1,55 @@
 package com.example.member;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class TempPassword {
 	
-	private String id;
-	private String temPassword;
+	private long id;
+	private String tempPassword;
 	
-	public String getId() {
+	public long getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(long id) {
 		this.id = id;
 	}
-	public String getTemPasswd() {
-		return temPassword;
+	public String getTempPassword() {
+		return tempPassword;
 	}
-	public void setTemPasswd(String temPasswd) {
-		this.temPassword = temPasswd;
+	public void setTempPassword(String tempPassword) {
+		this.tempPassword = tempPassword;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((tempPassword == null) ? 0 : tempPassword.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TempPassword other = (TempPassword) obj;
+		if (id != other.id)
+			return false;
+		if (tempPassword == null) {
+			if (other.tempPassword != null)
+				return false;
+		} else if (!tempPassword.equals(other.tempPassword))
+			return false;
+		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "TempPassword [id=" + id + ", temPasswd=" + temPassword + "]";
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
 	}
 	
-
 }
