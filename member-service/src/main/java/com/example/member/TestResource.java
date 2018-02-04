25310d3fd1671ae31cdbde4c37c62a6f36d0eb22
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2018-02-04 23:10:23 +0900

    Proxy Routing 시연을 위한 Test Resource 추가

---
+++ member-service/src/main/java/com/example/member/TestResource.java
@@ -0,0 +1,26 @@
+package com.example.member;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestResource {
+
+    @Value("${eureka.instance.metadataMap.instanceId}")
+    private String instanceId;
+
+    @GetMapping(path = "test")
+    public String testGet() {
+        return this.instanceId;
+    }
+
+    @PostMapping(path = "test")
+    public Map<String, Object> testPost(@RequestBody Map<String, Object> param) {
+        return param;
+    }
+}
