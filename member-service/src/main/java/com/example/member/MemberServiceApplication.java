a4ae171b4af6982fbd6ecb8ab50fa7564959066b
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2018-02-04 23:07:38 +0900

    Discovery Client 관련 설정 추가

--- member-service/src/main/java/com/example/member/MemberServiceApplication.java
+++ member-service/src/main/java/com/example/member/MemberServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.member;
 import com.example.core.log.RequestLogConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(RequestLogConfig.class)
+@EnableEurekaClient
 public class MemberServiceApplication {
 
 	public static void main(String[] args) {
--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.reservationservice;
 import com.example.core.log.RequestLogConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(RequestLogConfig.class)
+@EnableEurekaClient
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
